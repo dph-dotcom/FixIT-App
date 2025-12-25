f210f4cb3d59ccdec830aad445aa16bc79094e8a
dph-dotcom <dpherramia@gmail.com>
2025-12-25 16:50:50 +0800

    index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,8 @@
-import "../styles/globals.css"
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>RepairHub</h1>
+      <p>Find trusted repair professionals near you</p>
+    </main>
+  )
 }
-
